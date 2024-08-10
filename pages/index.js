27be613a52e4c2340b798a50df7ba4408a1cb04d
Jordi Aguilera Zamora <117550913+jordi-az@users.noreply.github.com>
2024-08-10 12:42:22 +0200

    modify index.js test push4

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
 
   return (
     <>
-      <header className="flex justify-center items-center mx-auto sticky bottom-0 md:top-0 w-full md:w-[720px] py-5 z-50">
+      <header className="flex justify-center items-center mx-auto sticky bottom-0 md:top-0 w-full max-w-[720px] py-5 z-50">
         <nav className="hidden md:flex gap-1 bg-gray-900/50 backdrop-blur-md px-4 py-0.5 rounded-full text-xl items-center">
           <Link
             href="/#sobre-mi"
@@ -89,18 +89,18 @@ export default function Home() {
         </nav>
       </header>
 
-      <main class="relative px-4">
+      <main className="relative px-4 max-w-full">
         <section
           id="sobre-mi"
-          class="w-full md:w-[720px] container mx-auto pt-4 pb-20"
+          className="w-full max-w-[720px] mx-auto pt-4 pb-20"
         >
-          <article class="mt-8 mb-8">
+          <article className="mt-8 mb-8">
             <Image
               src="/IMG_0368.jpeg"
               alt="Jordi Aguilera"
               width={124}
               height={124}
-              className="rounded-full img-profile img-profile:hover mb-8"
+              className="rounded-full img-profile mb-8"
             />
             <h2 className="flex gap-2 items-center text-4xl md:text-5xl font-extrabold mb-8">
               <svg
@@ -135,98 +135,12 @@ export default function Home() {
             </p>
           </article>
         </section>
-        <section class="w-full md:w-[720px] container mx-auto pt-4 pb-32">
-          <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-            <Image
-              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-              src="/next.svg"
-              alt="Next.js Logo"
-              width={180}
-              height={37}
-              priority
-            />
-          </div>
 
-          <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-            <Link
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-                Docs{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                  -&gt;
-                </span>
-              </h2>
-              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Find in-depth information about Next.js features and API.
-              </p>
-            </Link>
-
-            <Link
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-                Learn{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                  -&gt;
-                </span>
-              </h2>
-              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Learn about Next.js in an interactive course with&nbsp;quizzes!
-              </p>
-            </Link>
-
-            <Link
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-                Templates{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                  -&gt;
-                </span>
-              </h2>
-              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Explore starter templates for Next.js.
-              </p>
-            </Link>
-
-            <Link
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2 className={`mb-3 text-2xl font-semibold`}>
-                Deploy{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                  -&gt;
-                </span>
-              </h2>
-              <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                Instantly deploy your Next.js site to a shareable URL with
-                Vercel.
-              </p>
-            </Link>
-          </div>
-        </section>
-
-        <section
-          id="proyectos"
-          class="w-full md:w-[720px] container mx-auto py-8"
-        >
-          <h2 class="flex gap-2 items-center text-4xl md:text-5xl font-extrabold mb-8">
+        <section id="proyectos" className="w-full max-w-[720px] mx-auto py-8">
+          <h2 className="flex gap-2 items-center text-4xl md:text-5xl font-extrabold mb-8">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="size-10 md:size-12"
+              className="size-10 md:size-12"
               width="24"
               height="24"
               viewBox="0 0 24 24"
@@ -250,13 +164,13 @@ export default function Home() {
 
       <footer
         id="contacto"
-        class="md:w-[720px] container mx-auto pb-8 px-4 z-20 md:px-0"
+        className="w-full max-w-[720px] mx-auto pb-8 px-4 z-20 md:px-0"
       >
-        <div class="mb-8">
-          <h2 class="flex gap-2 items-center text-4xl md:text-5xl font-extrabold mb-8">
+        <div className="mb-8">
+          <h2 className="flex gap-2 items-center text-4xl md:text-5xl font-extrabold mb-8">
             {' '}
             <svg
-              class="size-10 md:size-12"
+              className="size-10 md:size-12"
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
@@ -281,13 +195,14 @@ export default function Home() {
             </svg>
             <span>Contacto</span>{' '}
           </h2>
-          <div class="flex items-center space-x-4">
-            <span class="text-2xl font-mono bg-gray-200 text-black border-2 border-slate-700 rounded-xl px-4 py-2 shadow-lg w-full ">
+          <div className="flex w-full items-center space-x-4">
+            <span className="flex-grow text-center text-base md:text-2xl font-mono bg-gray-200 text-black border-2 border-slate-700 rounded-xl px-4 py-2 shadow-lg block">
               jordi_duck_0@outlook.com
             </span>
+
             <Link
               href="mailto:jordi_duck_0@outlook.com"
-              class="relative px-0 py-0 group rounded-lg grid place-content-center"
+              className="flex-shrink-0 px-0 py-0 group rounded-lg grid place-content-center"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -304,13 +219,14 @@ export default function Home() {
                 <path d="M4.698 4.034l16.302 7.966l-16.302 7.966a.503 .503 0 0 1 -.546 -.124a.555 .555 0 0 1 -.12 -.568l2.468 -7.274l-2.468 -7.274a.555 .555 0 0 1 .12 -.568a.503 .503 0 0 1 .546 -.124z"></path>
                 <path d="M6.5 12h14.5"></path>
               </svg>
-              <span class="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-12 px-4 py-2 rounded-xl bg-white text-black w-32 transition-opacity duration-200 ease-out opacity-0 group-hover:opacity-100 text-center">
+              <span className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-12 px-4 py-2 rounded-xl bg-white text-black w-32 transition-opacity duration-200 ease-out opacity-0 group-hover:opacity-100 text-center">
                 Enviar email
               </span>
             </Link>
+
             <button
               id="button-copy-email"
-              class="relative px-0 py-0 group rounded-lg grid place-content-center"
+              className="flex-shrink-0 px-0 py-0 group rounded-lg grid place-content-center"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -329,7 +245,7 @@ export default function Home() {
               </svg>
               <span
                 id="tooltip-email"
-                class="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-12 px-4 py-2 rounded-xl bg-white text-black w-32 transition-opacity duration-200 ease-out opacity-0 group-hover:opacity-100 text-center"
+                className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-12 px-4 py-2 rounded-xl bg-white text-black w-32 transition-opacity duration-200 ease-out opacity-0 group-hover:opacity-100 text-center"
               >
                 Copiar email
               </span>
@@ -337,7 +253,7 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <h2 class="flex gap-2 items-center text-4xl md:text-5xl font-extrabold mb-8">
+          <h2 className="flex gap-2 items-center text-4xl md:text-5xl font-extrabold mb-8">
             {' '}
             <svg
               xxmlns="http://www.w3.org/2000/svg"
@@ -354,7 +270,7 @@ export default function Home() {
             </svg>
             <span>Discord</span>{' '}
           </h2>
-          <div class="grid grid-cols-8 flex-wrap md:flex-nowrap items-center gap-3"></div>
+          <div className="grid grid-cols-8 flex-wrap md:flex-nowrap items-center gap-3"></div>
         </div>
       </footer>
     </>
